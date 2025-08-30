b227a9c60fbc5f4d053ab8c3b25da29e40f4faa5
ZelhaDev <joshsaeed45@Gmail.com>
2025-08-31 01:05:47 +0800

    Changed var names to "marketmate" instead of "works"

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:works/main.dart';
+import 'package:marketmate/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
